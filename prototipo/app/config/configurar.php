3f9ae708b5630196d4b458a9e78bd24153e5e034
desarrollador <senanov2018@gmail.com>
2018-11-20 22:08:35 -0500

    Se modifica el .htaccess para tener un mejor manejo en las URL dentro del aplicativo

---
+++ prototipo/app/config/configurar.php
@@ -0,0 +1,19 @@
+<?php
+
+#Configuración de acceso a la base de datos 
+define('DB_HOST','localhost');
+define('DB_USUARIO','root');
+define('DB_PASSWORD','');
+define('DB_NOMBRE','senanov_db');
+
+#Ruta de la aplicación
+define('RUTA_APP',dirname(dirname(__FILE__)));
+
+#Ruta url ejemplo: http://localhost/senanov3.5/
+
+define('RUTA_URL','http://localhost/senanov3.5');
+
+define('NOM_SITIO','Senanov');
+
+define('SUPER_ADMINISTRADOR','1007382235');
+
